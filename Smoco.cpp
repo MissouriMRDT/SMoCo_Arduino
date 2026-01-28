788ac4c4b415721d92fa7297d36dd507bca20448
Angel <aespinoza1737@gmail.com>
2026-01-27 22:01:14 -0600

    echoRequest fix numba 2

--- Smoco.cpp
+++ Smoco.cpp
@@ -63,11 +63,11 @@ void Smoco::readIncomingMessage(CANMessage msg){
             // echo reply
             if(m_isPinging){
                 m_pingTime = 0;
-                m_pingTime = millis() - ((uint64_t)msg.data[0] | ((uint64_t)msg.data[1] << 8) | ((uint64_t)msg.data[2] << 16) | ((uint64_t)msg.data[3] << 24) | ((uint64_t)msg.data[4] << 32) | ((uint64_t)msg.data[5] << 40) | ((uint64_t)msg.data[6] << 48) | ((uint64_t)msg.data[7] << 56));
+                m_pingTime = millis() - (*(uint64_t)msg.data);
                 m_isPinging = false;
             }
             else{
-                m_echoData = msg.data[0] | ((uint64_t)msg.data[1] << 8) | ((uint64_t)msg.data[2] << 16) | ((uint64_t)msg.data[3] << 24) | ((uint64_t)msg.data[4] << 32) | ((uint64_t)msg.data[5] << 40) | ((uint64_t)msg.data[6] << 48) | ((uint64_t)msg.data[7] << 56);
+                m_echoData = (*(uint4_t)msg.data);
             }
             break;
         }
@@ -296,4 +296,5 @@ bool Smoco::getLimitSwitchBVariable(){
 bool Smoco::getEchoDataVariable(){
     return m_echoData;
 }
+
 
