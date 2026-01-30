70c95083beec037688878ababc69ef193fb3f81b
Brendan Westley <brendan@westley.us>
2026-01-30 00:23:22 -0600

    Update implementation

--- Smoco.cpp
+++ Smoco.cpp
@@ -1,307 +1,209 @@
 #include "Smoco.h"
 
-Smoco::Smoco(ACAN_T4 *canBus, uint8_t canID){
-        m_canBus = canBus;
-        m_canID = canID;
+Smoco::Smoco(ACAN_T4 *canBus, uint8_t canID)
+    : m_canBus(canBus), m_canID(canID) {}
+
+void Smoco::sync(CANMessage message) {
+  if (message.id >> 4 == 0x7F) {
+    // Update debug telemetry.
+    if ((message.id & 0xF) <= 8)
+      ((uint64_t *)&m_debugTelemetry)[message.id & 0xF] = message.data64;
+    return;
+  }
+  if (message.id >> 4 != m_canID)
+    return; // Not this SMoCo.
+
+  SmocoCANMessage *smocoCANMessage = (SmocoCANMessage *)message.data;
+  if (message.rtr) {
+    // Send missing parameter.
+    switch (message.id & 0xF) {
+    case MESSAGE_ID_SMOOTHING:
+      sendLowPassSmoothingFactor();
+      break;
+    case MESSAGE_ID_PID:
+      sendPID();
+      break;
+    case MESSAGE_ID_SOFT_LIMIT:
+      sendSoftLimitPosition();
+      break;
     }
-
-void Smoco::setCanID(uint8_t canID) {
-    m_canID = canID;
-}
-
-enum MessageID {
-    MESSAGE_ID_POSITION = 0x0,
-    MESSAGE_ID_POSITION_CALIBRATED = 0x1,
-    MESSAGE_ID_TARGET = 0x2,
-    MESSAGE_ID_SMOOTHING = 0x3,
-    MESSAGE_ID_PID = 0x4,
-    MESSAGE_ID_SOFT_LIMIT = 0x5,
-    MESSAGE_ID_CALIBRATE = 0x6,
-    MESSAGE_ID_STOP = 0xC,
-    MESSAGE_ID_ERROR = 0xD,
-    MESSAGE_ID_ECHO_REQUEST = 0xE,
-    MESSAGE_ID_ECHO_REPLY = 0xF,
-};
-
-enum TargetID {
-    TARGET_ID_OPEN_LOOP = 0x0,
-    TARGET_ID_POSITION = 0x2,
-    TARGET_ID_VELOCITY = 0x4,
-    TARGET_ID_CURRENT = 0x6,
-};
-
-void Smoco::readIncomingMessage(CANMessage msg){
-        switch( (uint8_t)((msg.id & 0xF)) ){
-        case MESSAGE_ID_POSITION: {
-            // position telemetry (angle, angular velocity, current, and limits)
-            // assigns telemetry values to member variables
-            m_angle = 0;
-            m_angle = msg.data[0] | (msg.data[1] << 8) | (msg.data[2] << 16) | (msg.data[3] << 24);
-
-            m_angularVelocity = msg.data[4] | (msg.data[5] << 8);
-
-            m_current = msg.data[6];
-
-            m_limSwitchA = msg.data[7] & 0b1000;
-            m_limSwitchB = msg.data[7] & 0b0100;
-            m_softLimA = msg.data[7] & 0b0010;
-            m_softLimB = msg.data[7] & 0b0001;
-            break;
-        }
-        case MESSAGE_ID_POSITION_CALIBRATED: {
-            // position calbirated return status
-            // calibration finished
-            m_isCalibrated = true;
-            break;
-        }
-        case MESSAGE_ID_ERROR: {
-            // command error; returns ID of failed can msg
-            m_commandErrorID = msg.data[0];
-            break;
-        }
-        case MESSAGE_ID_ECHO_REPLY: {
-            // echo reply
-            if(m_isPinging){
-                m_pingTime = 0;
-                m_pingTime = millis() - (*(uint64_t*)msg.data);
-                m_isPinging = false;
-            }
-            else{
-                m_echoData = (*(uint64_t*)msg.data);
-            }
-            break;
-        }
-        }
+  } else {
+    switch (message.id & 0xF) {
+    case MESSAGE_ID_POSITION:
+      m_position = smocoCANMessage->position.position;
+      m_velocity = smocoCANMessage->position.velocity;
+      m_current = smocoCANMessage->position.current;
+      // Get the bit at each position and convert it to a bool.
+      m_limitSwitchA = !!(smocoCANMessage->position.flags & (1 << 7));
+      m_limitSwitchB = !!(smocoCANMessage->position.flags & (1 << 6));
+      m_softLimitA = !!(smocoCANMessage->position.flags & (1 << 5));
+      m_softLimitB = !!(smocoCANMessage->position.flags & (1 << 4));
+      break;
+    case MESSAGE_ID_POSITION_CALIBRATED:
+      m_calibrated = true;
+      break;
+    case MESSAGE_ID_ERROR:
+      m_commandErrorID = smocoCANMessage->commandError.commandID;
+      break;
+    case MESSAGE_ID_ECHO_REPLY:
+      m_echoResponse = smocoCANMessage->echoReply.payload;
+      m_pingTime = millis() - m_echoResponse;
+      break;
     }
-
-void Smoco::openLoopDrive(int16_t dutyCycle, bool ignoreLimit) {
-    CANMessage msg;
-    msg.id = (m_canID << 4) | MESSAGE_ID_TARGET;
-    m_ignoreLimit = ignoreLimit;
-    msg.data[0] = m_ignoreLimit ? TARGET_ID_OPEN_LOOP + 1 : TARGET_ID_OPEN_LOOP;
-
-    msg.data[1] = (uint8_t)(dutyCycle & 0xFF);
-    msg.data[2] = (uint8_t)((dutyCycle >> 8) & 0xFF);
-
-    msg.len = 3;
-    const bool ok = m_canBus->tryToSend(msg);
+  }
 }
 
-void Smoco::setJointAngle(uint32_t targetPosition, uint16_t errorGain, bool ignoreLimit) {
-    CANMessage msg;
-    msg.id = (m_canID << 4) | MESSAGE_ID_TARGET;
-    m_ignoreLimit = ignoreLimit;
-    msg.data[0] = m_ignoreLimit? TARGET_ID_POSITION + 1: TARGET_ID_OPEN_LOOP;
-
-    msg.data[1] = (uint8_t)(errorGain & 0xFF);
-    msg.data[2] = (uint8_t)((errorGain >> 8) & 0xFF);
-
-    msg.data[3] = (uint8_t)(targetPosition & 0xFF);
-    msg.data[4] = (uint8_t)((targetPosition >> 8) & 0xFF);
-    msg.data[5] = (uint8_t)((targetPosition >> 16) & 0xFF);
-    msg.data[6] = (uint8_t)((targetPosition >> 24) & 0xFF);
-
-    msg.len = 7;
-    const bool ok = m_canBus->tryToSend(msg);
+bool Smoco::driveOpenLoop(int16_t dutyCycle, bool ignoreLimit) {
+  m_dutyCycle = dutyCycle;
+  m_ignoreLimit = !!ignoreLimit;
+  return m_canBus->tryToSend(CANMessage{
+      .id = (m_canID << 4) | MESSAGE_ID_TARGET,
+      .len = 3,
+      .data64 =
+          SmocoCANMessage{
+              .target = {.sid = MESSAGE_SID_OPEN_LOOP + (uint8_t)m_ignoreLimit,
+                         .openLoop = {.dutyCycle = m_dutyCycle}}}
+              .data64});
 }
 
-void Smoco::setJointVelocity(uint32_t targetVelocity, uint16_t errorGain, bool ignoreLimit = false) {
-    CANMessage msg;
-    msg.id = (m_canID << 4) | MESSAGE_ID_TARGET;
-    m_ignoreLimit = ignoreLimit;
-    msg.data[0] = m_ignoreLimit ? TARGET_ID_VELOCITY + 1 : TARGET_ID_VELOCITY;
-
-    msg.data[1] = (uint8_t)(errorGain & 0xFF);
-    msg.data[2] = (uint8_t)((errorGain >> 8) & 0xFF);
-    
-    msg.data[3] = (uint8_t)(targetVelocity & 0xFF);
-    msg.data[4] = (uint8_t)((targetVelocity >> 8) & 0xFF);
-    msg.data[5] = (uint8_t)((targetVelocity >> 16) & 0xFF);
-    msg.data[6] = (uint8_t)((targetVelocity >> 24) & 0xFF);
-
-    msg.len = 7;
-    const bool ok = m_canBus->tryToSend(msg);
+bool Smoco::driveTargetPosition(int32_t targetPosition, uint16_t errorGain,
+                                bool ignoreLimit) {
+  m_targetPosition = targetPosition;
+  m_errorGain = errorGain;
+  m_ignoreLimit = !!ignoreLimit;
+  return m_canBus->tryToSend(CANMessage{
+      .id = (m_canID << 4) | MESSAGE_ID_TARGET,
+      .len = 7,
+      .data64 =
+          SmocoCANMessage{
+              .target = {.sid = MESSAGE_SID_POSITION + (uint8_t)m_ignoreLimit,
+                         .targetPosition = {.errorGain = m_errorGain,
+                                            .position = m_targetPosition}}}
+              .data64});
 }
 
-void Smoco::setJointCurrent(int16_t targetCurrent, uint16_t errorGain, bool ignoreLimit = false) {
-    CANMessage msg;
-    msg.id = (m_canID << 4) | MESSAGE_ID_TARGET;
-    m_ignoreLimit = ignoreLimit;
-    msg.data[0] = m_ignoreLimit ? TARGET_ID_CURRENT + 1 : TARGET_ID_CURRENT;
-
-    msg.data[1] = (uint8_t)(errorGain & 0xFF);
-    msg.data[2] = (uint8_t)((errorGain >> 8) & 0xFF);
-
-    msg.data[3] = (uint8_t)(targetCurrent & 0xFF);
-    msg.data[4] = (uint8_t)((targetCurrent >> 8) & 0xFF);
-
-    msg.len = 5;
-    const bool ok = m_canBus->tryToSend(msg);
+bool Smoco::driveTargetVelocity(int32_t targetVelocity, uint16_t errorGain,
+                                bool ignoreLimit) {
+  m_targetVelocity = targetVelocity;
+  m_errorGain = errorGain;
+  m_ignoreLimit = !!ignoreLimit;
+  return m_canBus->tryToSend(CANMessage{
+      .id = (m_canID << 4) | MESSAGE_ID_TARGET,
+      .len = 7,
+      .data64 =
+          SmocoCANMessage{
+              .target = {.sid = MESSAGE_SID_VELOCITY + (uint8_t)m_ignoreLimit,
+                         .targetVelocity = {.errorGain = m_errorGain,
+                                            .velocity = m_targetVelocity}}}
+              .data64});
 }
 
-void Smoco::setLowPassSmoothingFactor(uint16_t alpha) {
-    CANMessage msg;
-    msg.id = (m_canID << 4) | MESSAGE_ID_SMOOTHING;
-
-    msg.data[0] = (uint8_t)(alpha & 0xFF);
-    msg.data[1] = (uint8_t)((alpha >> 8) & 0xFF);
-
-    msg.len = 2;
-    const bool ok = m_canBus->tryToSend(msg);
+bool Smoco::driveTargetCurrent(int16_t targetCurrent, uint16_t errorGain,
+                               bool ignoreLimit) {
+  m_targetCurrent = targetCurrent;
+  m_errorGain = errorGain;
+  m_ignoreLimit = !!ignoreLimit;
+  return m_canBus->tryToSend(CANMessage{
+      .id = (m_canID << 4) | MESSAGE_ID_TARGET,
+      .len = 5,
+      .data64 =
+          SmocoCANMessage{
+              .target = {.sid = MESSAGE_SID_CURRENT + (uint8_t)m_ignoreLimit,
+                         .targetCurrent = {.errorGain = m_targetCurrent,
+                                           .current = targetCurrent}}}
+              .data64});
 }
 
-void Smoco::setPID(uint16_t P, uint16_t I, uint16_t D) {
-    CANMessage msg;
-    msg.id = (m_canID << 4) | MESSAGE_ID_PID;
-
-    msg.data[0] = (uint8_t)(P & 0xFF);
-    msg.data[1] = (uint8_t)((P >> 8) & 0xFF);
-
-    msg.data[2] = (uint8_t)(I & 0xFF);
-    msg.data[3] = (uint8_t)((I >> 8) & 0xFF);
-
-    msg.data[4] = (uint8_t)(D & 0xFF);
-    msg.data[5] = (uint8_t)((D >> 8) & 0xFF);
-
-    msg.len = 6;
-    const bool ok = m_canBus->tryToSend(msg);
+bool Smoco::setLowPassSmoothingFactor(uint16_t alpha) {
+  m_lowPassSmoothingFactor = alpha;
+  return sendLowPassSmoothingFactor();
 }
 
-void Smoco::setSoftLimitPosition(int32_t positionA, int32_t positionB) {
-    CANMessage msg;
-    msg.id = (m_canID << 4) | MESSAGE_ID_SOFT_LIMIT;
-
-    msg.data[0] = (uint8_t)(positionA & 0xFF);
-    msg.data[1] = (uint8_t)((positionA >> 8) & 0xFF);
-    msg.data[2] = (uint8_t)((positionA >> 16) & 0xFF);
-    msg.data[3] = (uint8_t)((positionA >> 24) & 0xFF);
-
-    msg.data[4] = (uint8_t)(positionB & 0xFF);
-    msg.data[5] = (uint8_t)((positionB >> 8) & 0xFF);
-    msg.data[6] = (uint8_t)((positionB >> 16) & 0xFF);
-    msg.data[7] = (uint8_t)((positionB >> 24) & 0xFF);
-
-    msg.len = 8;
-    const bool ok = m_canBus->tryToSend(msg);
+bool Smoco::sendLowPassSmoothingFactor() {
+  return m_canBus->tryToSend(CANMessage{
+      .id = (m_canID << 4) | MESSAGE_ID_SMOOTHING,
+      .len = 2,
+      .data64 =
+          SmocoCANMessage{
+              .setLowPassSmoothingFactor = {.alpha = m_lowPassSmoothingFactor}}
+              .data64});
 }
 
-void Smoco::startPositionCalibration(int16_t dutyCycle, int32_t limitSwitchPosition) {
-    CANMessage msg;
-    msg.id = (m_canID << 4) | MESSAGE_ID_CALIBRATE;
-    msg.data[0] = (uint8_t)(dutyCycle & 0xFF);
-    msg.data[1] = (uint8_t)((dutyCycle >> 8) & 0xFF);
-
-    msg.data[2] = (uint8_t)(limitSwitchPosition & 0xFF);
-    msg.data[3] = (uint8_t)((limitSwitchPosition >> 8) & 0xFF);
-    msg.data[4] = (uint8_t)((limitSwitchPosition >> 16) & 0xFF);
-    msg.data[5] = (uint8_t)((limitSwitchPosition >> 24) & 0xFF);
-
-    msg.len = 6;
-    const bool ok = m_canBus->tryToSend(msg);
+bool Smoco::setPID(uint16_t P, uint16_t I, uint16_t D) {
+  m_PID[0] = P;
+  m_PID[1] = I;
+  m_PID[2] = D;
+  return sendPID();
 }
 
-void Smoco::debugTelemetry(uint8_t enable){
-    CANMessage msg;
-    msg.id = (m_canID << 4) | 7;
-    msg.data[0] = enable;
-
-    msg.len = 1;
-    const bool ok = m_canBus->tryToSend(msg);
+bool Smoco::sendPID() {
+  return m_canBus->tryToSend(CANMessage{
+      .id = (m_canID << 4) | MESSAGE_ID_PID,
+      .len = 6,
+      .data64 =
+          SmocoCANMessage{
+              .setPID = {.p = m_PID[0], .i = m_PID[1], .d = m_PID[2]}}
+              .data64});
 }
 
-void Smoco::stopAndReset() {
-    CANMessage msg;
-    msg.id = (m_canID << 4) | MESSAGE_ID_STOP;
-    msg.len = 0;
-    const bool ok = m_canBus->tryToSend(msg);
+bool Smoco::setSoftLimitPosition(int32_t positionA, int32_t positionB) {
+  m_softLimitAPosition = positionA;
+  m_softLimitBPosition = positionB;
+  return sendSoftLimitPosition();
 }
 
-void Smoco::echoRequest(uint64_t payload) {
-    CANMessage msg;
-    msg.id = (m_canID << 4) | MESSAGE_ID_ECHO_REQUEST;
-
-    msg.data[0] = (uint8_t)(payload & 0xFF);
-    msg.data[1] = (uint8_t)((payload >> 8) & 0xFF);
-    msg.data[2] = (uint8_t)((payload >> 16) & 0xFF);
-    msg.data[3] = (uint8_t)((payload >> 24) & 0xFF);
-    msg.data[4] = (uint8_t)((payload >> 32) & 0xFF);
-    msg.data[5] = (uint8_t)((payload >> 40) & 0xFF);
-    msg.data[6] = (uint8_t)((payload >> 48) & 0xFF);
-    msg.data[7] = (uint8_t)((payload >> 56) & 0xFF);
-
-    msg.len = 8;
-
-    const uint32_t status = m_canBus->tryToSendReturnStatus(msg);
-    if (status) {
-        Serial.print("OOPS! ");
-        Serial.println(status, HEX);
-    }
+bool Smoco::sendSoftLimitPosition() {
+  return m_canBus->tryToSend(CANMessage{
+      .id = (m_canID << 4) | MESSAGE_ID_SOFT_LIMIT,
+      .len = 8,
+      .data64 =
+          SmocoCANMessage{
+              .setSoftLimitPosition = {.aPosition = m_softLimitAPosition,
+                                       .bPosition = m_softLimitBPosition}}
+              .data64});
 }
 
-void Smoco::smocoPing(){
-    m_isPinging = true;
-    echoRequest(millis());
+bool Smoco::calibratePosition(int16_t dutyCycle, int32_t limitSwitchPosition) {
+  m_calibrationDutyCycle = dutyCycle;
+  m_calibrationPosition = limitSwitchPosition;
+  m_calibrated = false;
+  return m_canBus->tryToSend(CANMessage{
+      .id = (m_canID << 4) | MESSAGE_ID_CALIBRATE,
+      .len = 6,
+      .data64 =
+          SmocoCANMessage{
+              .startPositionCalibration = {.dutyCycle = m_calibrationDutyCycle,
+                                           .limitSwitchPosition =
+                                               m_calibrationPosition}}
+              .data64});
 }
 
-
-void Smoco::setAngleVariable(int32_t angle){
-    m_angle = angle;
+bool Smoco::debugTelemetry(uint8_t enable) {
+  m_debugTelemetryEnabled = enable;
+  return m_canBus->tryToSend(CANMessage{
+      .id = (m_canID << 4) | MESSAGE_ID_DEBUG,
+      .len = 1,
+      .data64 =
+          SmocoCANMessage{.debugTelemetry = {.enable = m_debugTelemetryEnabled}}
+              .data64});
 }
 
-void Smoco::setPIDVariables(uint16_t P, uint16_t I, uint16_t D){
-    m_PID[0] = P;
-    m_PID[1] = I;
-    m_PID[2] = D;
+bool Smoco::stopAndReset() {
+  return m_canBus->tryToSend(
+      CANMessage{.id = (m_canID << 4) | MESSAGE_ID_STOP, .len = 0});
 }
 
-void Smoco::setAlphaVariable(uint16_t alpha){
-    m_lowPassSmoothingAlpha = alpha;
+bool Smoco::echoRequest(uint64_t payload) {
+  m_echoRequestPayload = payload;
+  return m_canBus->tryToSend(CANMessage{
+      .id = (m_canID << 4) | MESSAGE_ID_ECHO_REQUEST,
+      .len = 8,
+      .data64 =
+          SmocoCANMessage{.echoRequestPayload = {.payload = m_echoResponse}}
+              .data64});
 }
 
-void Smoco::setSoftLimitAVariable(int32_t limitA){
-    m_softLimitA = limitA;
+bool Smoco::ping() {
+  m_pinging = true;
+  return echoRequest(millis());
 }
-
-void Smoco::setSoftLimitBVariable(int32_t limitB){
-    m_softLimitB = limitB;
-}
-
-void Smoco::setIgnoreLimitVariable(bool ignoreLim){
-    m_ignoreLimit = ignoreLim;
-}
-
-void Smoco::setPingTimeVariable(uint64_t pingTime){
-    m_pingTime = pingTime;
-}
-
-
-bool Smoco::getignoreLimitVariable(){
-    return m_ignoreLimit;
-}
-int32_t Smoco::getAngleVariable(){
-    return m_angle;
-}
-bool Smoco::getSoftLimitAVariable(){
-    return m_softLimA;
-}
-bool Smoco::getSoftLimitBVariable(){
-    return m_softLimB;
-}
-bool Smoco::getLimitSwitchAVariable(){
-   return m_limSwitchA;
-}
-bool Smoco::getLimitSwitchBVariable(){
-    return m_limSwitchB;
-}
-uint64_t Smoco::getEchoDataVariable(){
-    return m_echoData;
-}
-uint16_t Smoco::getPingTimeVariable(){
-    return m_pingTime;
-}
-
-
-
-
-
-
--- Smoco.h
+++ Smoco.h
@@ -3,95 +3,163 @@
 
 #include <ACAN_T4.h>
 
-enum class ControlMode { STOP, OPEN_LOOP, POSITION, VELOCITY, CURRENT, CALIBRATING };
+#define MESSAGE_ID_POSITION 0x0
+#define MESSAGE_ID_POSITION_CALIBRATED 0x1
+#define MESSAGE_ID_TARGET 0x2
+#define MESSAGE_ID_SMOOTHING 0x3
+#define MESSAGE_ID_PID 0x4
+#define MESSAGE_ID_SOFT_LIMIT 0x5
+#define MESSAGE_ID_CALIBRATE 0x6
+#define MESSAGE_ID_DEBUG 0x7
+#define MESSAGE_ID_STOP 0xC
+#define MESSAGE_ID_ERROR 0xD
+#define MESSAGE_ID_ECHO_REQUEST 0xE
+#define MESSAGE_ID_ECHO_REPLY 0xF
+
+#define MESSAGE_SID_OPEN_LOOP 0x0
+#define MESSAGE_SID_POSITION 0x2
+#define MESSAGE_SID_VELOCITY 0x4
+#define MESSAGE_SID_CURRENT 0x6
+
+enum class ControlMode {
+  STOP,
+  OPEN_LOOP,
+  POSITION,
+  VELOCITY,
+  CURRENT,
+  CALIBRATING
+};
+
+typedef union {
+  uint64_t data64;
+  struct __attribute__((__packed__)) {
+    int32_t position;
+    int16_t velocity;
+    uint8_t current;
+    uint8_t flags;
+  } position;
+  struct __attribute__((__packed__)) {
+    uint8_t commandID;
+  } commandError;
+  struct __attribute__((__packed__)) {
+    uint64_t payload;
+  } echoReply;
+  struct __attribute__((__packed__)) {
+    uint8_t sid;
+    union {
+      struct __attribute__((__packed__)) {
+        int16_t dutyCycle;
+      } openLoop;
+      struct __attribute__((__packed__)) {
+        uint16_t errorGain;
+        int32_t position;
+      } targetPosition;
+      struct __attribute__((__packed__)) {
+        uint16_t errorGain;
+        int32_t velocity;
+      } targetVelocity;
+      struct __attribute__((__packed__)) {
+        uint16_t errorGain;
+        int16_t current;
+      } targetCurrent;
+    };
+  } target;
+  struct __attribute__((__packed__)) {
+    uint16_t alpha;
+  } setLowPassSmoothingFactor;
+  struct __attribute__((__packed__)) {
+    uint16_t p;
+    uint16_t i;
+    uint16_t d;
+  } setPID;
+  struct __attribute__((__packed__)) {
+    int32_t aPosition;
+    int32_t bPosition;
+  } setSoftLimitPosition;
+  struct __attribute__((__packed__)) {
+    int16_t dutyCycle;
+    int32_t limitSwitchPosition;
+  } startPositionCalibration;
+  struct __attribute__((__packed__)) {
+    bool enable;
+  } debugTelemetry;
+  struct __attribute__((__packed__)) {
+    uint64_t payload;
+  } echoRequestPayload;
+} SmocoCANMessage;
+typedef struct __attribute__((__packed__)) {
+  uint64_t tick;
+  int64_t position;
+  double velocity;
+  double current;
+  double pOut;
+  double iOut;
+  double dOut;
+  double error;
+  double deltaT;
+} SmocoDebugTelemetry;
 
 class Smoco {
-
-private:
-
-    ACAN_T4 *m_canBus;
-    uint8_t m_canID;
-
-    //int16_t m_dutyCycle;
-
-    int32_t m_angle;
-    int16_t m_angularVelocity;
-    uint8_t m_current;
-
-    bool m_ignoreLimit;
-    uint16_t m_errorGain;
-    uint16_t m_lowPassSmoothingAlpha;
-
-    bool m_isCalibrated;
-
-    uint16_t m_PID[3] = {0,0,0};
-
-    int32_t m_softLimitA;
-    int32_t m_softLimitB;
-
-    bool m_limSwitchA;
-    bool m_limSwitchB;
-    bool m_softLimA;
-    bool m_softLimB;
-
-    uint16_t m_pingTime;
-
-    uint8_t m_commandErrorID; // :)
-    bool m_isPinging;
-
-    uint64_t m_echoData;
-
 public:
-    // constructor
-    Smoco(ACAN_T4 *canBus, uint8_t canID);
+  ACAN_T4 *m_canBus;
 
-    void setCanID(uint8_t canID);
+  uint32_t m_canID;
 
-    //void sendTelemetry();
+  int32_t m_position;
+  int16_t m_velocity;
+  uint8_t m_current;
 
-    void openLoopDrive(int16_t dutyCycle, bool ignoreLimit = false);
-    void setJointAngle(uint32_t targetAngle, uint16_t errorGain, bool ignoreLimit = false);
-    void setJointVelocity(uint32_t targetVelocity, uint16_t errorGain, bool ignoreLimit = false);
-    void setJointCurrent(int16_t targetCurrent, uint16_t errorGain, bool ignoreLimit = false);
-    void setLowPassSmoothingFactor(uint16_t alpha);
-    void setPID(uint16_t P, uint16_t I, uint16_t D);
-    void setSoftLimitPosition(int32_t positionA, int32_t positionB);
-    void startPositionCalibration(int16_t dutyCycle, int32_t limitSwitchPosition);
-    void debugTelemetry(uint8_t enable);
-    void stopAndReset();
-    void echoRequest(uint64_t payload);
-    void readIncomingMessage(CANMessage msg);
-    void smocoPing();
+  bool m_ignoreLimit;
+  int16_t m_dutyCycle;
+  uint16_t m_errorGain;
+  int32_t m_targetPosition;
+  int32_t m_targetVelocity;
+  uint16_t m_targetCurrent;
+  uint16_t m_lowPassSmoothingFactor;
+  uint16_t m_PID[3];
+  int32_t m_softLimitAPosition;
+  int32_t m_softLimitBPosition;
+  int16_t m_calibrationDutyCycle;
+  int32_t m_calibrationPosition;
+  bool m_debugTelemetryEnabled;
 
-    // Setters
-    void setAngleVariable(int32_t angle);
-    void setPIDVariables(uint16_t P, uint16_t I, uint16_t D);
-    void setAlphaVariable(uint16_t alpha);
-    void setSoftLimitAVariable(int32_t limitA);
-    void setSoftLimitBVariable(int32_t limitB);
-    void setIgnoreLimitVariable(bool ignoreLim);
-    void setPingTimeVariable(uint64_t pingTIme);
+  bool m_calibrated;
 
-    // getters
-    int32_t getAngleVariable();
-    bool getignoreLimitVariable();
+  bool m_limitSwitchA;
+  bool m_limitSwitchB;
+  bool m_softLimitA;
+  bool m_softLimitB;
 
-    bool getSoftLimitAVariable();
-    bool getSoftLimitBVariable();
-    bool getLimitSwitchAVariable();
-    bool getLimitSwitchBVariable();
-    uint64_t getEchoDataVariable();
-    uint16_t getPingTimeVariable();
+  uint64_t m_echoRequestPayload;
+  uint64_t m_echoResponse;
+  bool m_pinging;
+  uint64_t m_pingTime;
+
+  uint8_t m_commandErrorID;
+
+  SmocoDebugTelemetry m_debugTelemetry;
+
+  bool sendLowPassSmoothingFactor();
+  bool sendPID();
+  bool sendSoftLimitPosition();
+
+  Smoco(ACAN_T4 *canBus, uint8_t canID);
+
+  bool driveOpenLoop(int16_t dutyCycle, bool ignoreLimit = false);
+  bool driveTargetPosition(int32_t targetPosition, uint16_t errorGain,
+                           bool ignoreLimit = false);
+  bool driveTargetVelocity(int32_t targetVelocity, uint16_t errorGain,
+                           bool ignoreLimit = false);
+  bool driveTargetCurrent(int16_t targetCurrent, uint16_t errorGain,
+                          bool ignoreLimit = false);
+  bool setLowPassSmoothingFactor(uint16_t alpha);
+  bool setPID(uint16_t P, uint16_t I, uint16_t D);
+  bool setSoftLimitPosition(int32_t positionA, int32_t positionB);
+  bool calibratePosition(int16_t dutyCycle, int32_t limitSwitchPosition);
+  bool debugTelemetry(uint8_t enable);
+  bool stopAndReset();
+  bool echoRequest(uint64_t payload);
+  void sync(CANMessage msg);
+  bool ping();
 };
 #endif
-
-
-
-
-
-
-
-
-
-
-
